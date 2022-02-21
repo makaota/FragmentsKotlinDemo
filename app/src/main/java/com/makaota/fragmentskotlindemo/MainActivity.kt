1ee25df106502ca52f17d2131c21fbcd6be49bc6
makaota <74915165+makaota@users.noreply.github.com>
2022-02-21 15:24:42 +0200

    Completed the fragment lifecycle demo app

--- app/src/main/java/com/makaota/fragmentskotlindemo/MainActivity.kt
+++ app/src/main/java/com/makaota/fragmentskotlindemo/MainActivity.kt
@@ -2,10 +2,19 @@ package com.makaota.fragmentskotlindemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null){
+            supportFragmentManager.commit {
+                setReorderingAllowed(true)
+                add<ExampleFragment>(R.id.fragment_container_view)
+            }
+        }
     }
 }
